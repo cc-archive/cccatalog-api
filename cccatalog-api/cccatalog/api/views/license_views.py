43a2d50b232cf4565e5740e221a24e46c1a73aff
Ritesh Pandey <ritpan2003@gmail.com>
2019-10-22 16:28:29 +0530

    Use licenses.py file to create response
    licenses.py contains list of liceses used by system. Use that file to create response to GET license request

--- cccatalog-api/cccatalog/api/views/license_views.py
+++ cccatalog-api/cccatalog/api/views/license_views.py
@@ -1,25 +1,18 @@
-from django.core import serializers
-from django.http import HttpResponse
-from django.views import View
-from django.views.generic import ListView
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from cccatalog.api.models import Image
-from cccatalog.api.serializers.license_serializers import LicenseSerializer
+from cccatalog.api.licenses import LICENSES, LICENSE_URL
 
 class ListLicense(APIView):
     """
     List all licenses available in catalog
     """
-    @swagger_auto_schema(operation_id='license',
-                         query_serializer=LicenseSerializer,
-                         responses={
-                             200: LicenseSerializer(many=True)
-                         })
+    @swagger_auto_schema(operation_id='license')
     def get(self, request):
-        images = Image.objects.order_by('license', 'license_version').distinct('license', 'license_version')
-        serialized_image = LicenseSerializer(images, many=True)
-        return Response(data=serialized_image.data)
+        response = []
+        for license in LICENSES:
+          response.append({'license': license[0], 'license_version': 2, 'url':
+          LICENSE_URL})
+
+        return Response(data=response)
