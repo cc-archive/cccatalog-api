9cca35eecdf32a4884820cd930e104e7f662ba43
Khyati Agarwal <30500382+khyati-agarwalss@users.noreply.github.com>
2020-02-15 01:06:31 +0530

    Replaced 'provider' occurences with 'source'

--- ccbot/crawl_planner/test/make_test_url_dump.py
+++ ccbot/crawl_planner/test/make_test_url_dump.py
@@ -2,8 +2,8 @@ import csv
 from uuid import uuid4
 
 with open('test_url_dump.csv', 'w') as csvfile, open('pagelist', 'r') as pages:
-    writer = csv.DictWriter(csvfile, fieldnames=['url','identifier','provider'])
+    writer = csv.DictWriter(csvfile, fieldnames=['url','identifier','source'])
     writer.writeheader()
     for page in pages.readlines():
         page = page.rstrip()
-        writer.writerow({'url': 'http://localhost:8080/' + page, 'identifier': str(uuid4()), 'provider': 'localhost'})
+        writer.writerow({'url': 'http://localhost:8080/' + page, 'identifier': str(uuid4()), 'source': 'localhost'})
