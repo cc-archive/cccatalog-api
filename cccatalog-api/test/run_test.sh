5d384f3d775141b1e144493104dc9d7032ee57b4
Alden S. Page <alden@alden.page>
2020-08-18 15:45:51 -0400

    Move log dump to test script

--- cccatalog-api/test/run_test.sh
+++ cccatalog-api/test/run_test.sh
@@ -3,3 +3,4 @@
 export PYTHONWARNINGS="ignore:Unverified HTTPS request"
 export INTEGRATION_TEST_URL="http://localhost:8000"
 DJANGO_SETTINGS_MODULE='cccatalog.settings' PYTHONPATH=. DJANGO_SECRET_KEY='ny#b__$f6ry4wy8oxre97&-68u_0lk3gw(z=d40_dxey3zw0v1' DJANGO_DATABASE_NAME='openledger' DJANGO_DATABASE_USER='deploy' DJANGO_DATABASE_PASSWORD='deploy' DJANGO_DATABASE_HOST='localhost' REDIS_HOST='localhost' pytest -s --disable-pytest-warnings test/v1_integration_test.py
+docker-compose logs
